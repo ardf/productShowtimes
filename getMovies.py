5ed39fb0a6369d58ba911448183e9ebf589fcea5
ardf <ruyefadf@gmail.com>
2022-04-02 18:27:33 +0530

    Designed apt database schema

--- getMovies.py
+++ getMovies.py
@@ -6,14 +6,14 @@ ia = Cinemagoer()
 top = ia.get_popular100_movies()
 movie_list = []
 for movie in top[:25]:
-    movie_id = movie.movieID
+    id = movie.movieID
     title = movie.data['title']
     year = movie.data['year']
-    movie_list.append((movie_id, title, year))
+    movie_list.append((id, title, year))
 
 con = sqlite3.connect('product_showtimes.db')
 cur = con.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS latest_shows (id Numeric Primary Key, title text, year integer)""")
-cur.executemany("INSERT INTO latest_shows VALUES (?, ?, ?)", movie_list)
+cur.execute("""CREATE TABLE IF NOT EXISTS movies (id Numeric Primary Key, title text, year integer)""")
+cur.executemany("INSERT OR IGNORE INTO movies VALUES (?, ?, ?)", movie_list)
 con.commit()
 con.close()
